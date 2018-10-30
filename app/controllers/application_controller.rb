82ae6d2a7f415cfe7bfbdf3fbe558cc93c801577
yli0607x <github email address>
2018-10-30 17:33:02 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,61 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+
+  #NEW
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #INDEX
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  #SHOW
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+
+  #EDIT
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  #UPDATE
+  # get 'recipes/:id' do
+  #   @recipe = Recipe.find(params[:id])
+  #   @recipe.update(params[:recipe])
+  #   @recipe.save
+  #   redirect :"recipes/#{@recipe.id}"
+  # end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  #CREATE
+
+  post '/recipes' do  #creates a recipe
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    erb :delete
+  end
+
 end
